ddf93d5903e229f8d7fae8b042022f11693d6e46
santosomar <os@cisco.com>
2023-02-10 02:10:20 +0000

    adding the ability to search

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 ![Python versions](https://img.shields.io/pypi/pyversions/danger-python)
 
 # KEV Checker
-A basic Python program to check Cybersecurity & Infrastructure Security Agency (CISA) [Known Exploited Vulnerabilities (KEV) Catalog](https://www.cisa.gov/known-exploited-vulnerabilities-catalog).
-
+his script retrieves and processes the Known Exploited Vulnerabilities (KEV) data from the CISA website. The data is stored in a CSV file and is processed using the pandas library.
 
 ## Requirements
 - Python 3
-- requests
 - pandas
+- requests
+- argparse
 
+## Usage
+```
+python3 kev_data_processing.py <search>
+<search> can be either a cveID, vendorProject, product, or a time range in the format "YYYY-MM-DD:YYYY-MM-DD".
+```     
 
+## Output
+The script outputs the filtered records that match the search term, including the "cveID", "vendorProject", "product", and "dateAdded" columns.
+
+## Example
+```
+python3 kev_data_processing.py 2023-01-01:2023-12-31
+```
+
+This command will retrieve and process the KEV data, and output all records that have a "dateAdded" field within the range of January 1, 2023 to December 31, 2023.
--- kev_checker.py
+++ kev_checker.py
@@ -2,6 +2,7 @@
 # Author: Omar Santos @santosomar
 # This script retrieves CISA's KEV data and processes the data using pandas
 
+import argparse
 import pandas as pd
 import requests
 from datetime import timedelta
@@ -31,18 +32,31 @@ column_names = df.columns
 # Convert the dateAdded field to a datetime object
 df["dateAdded"] = pd.to_datetime(df["dateAdded"])
 
-# Get the current date
-current_date = pd.Timestamp.now().date()
+# Use argparse to parse the command line arguments
+parser = argparse.ArgumentParser(description='Retrieve and process CISA KEV data.')
 
-# Calculate the date 15 days ago
-fifteen_days_ago = current_date - timedelta(days=15)
+parser.add_argument('search', type=str, help='The search term to use. Can be a cveID, vendorProject, product, or a time range in the format "YYYY-MM-DD:YYYY-MM-DD".')
 
-# Select the rows where dateAdded is greater than or equal to 15 days ago
-new_records = df[df["dateAdded"].dt.date >= fifteen_days_ago]
+args = parser.parse_args()
 
-# Print the new records
-print(new_records[["cveID", "vendorProject", "product"]])
+# Get the search term from the arguments
+search_input = args.search
 
+# Split the input into two parts, separated by a colon
+search_input_parts = search_input.split(":")
 
+# Check if the input is a time range
+if len(search_input_parts) == 2:
+    # Get the start and end date from the input
+    start_date = search_input_parts[0]
+    end_date = search_input_parts[1]
 
+    # Filter the DataFrame to only include rows where the dateAdded is within the time range
+    filtered_df = df[(df["dateAdded"] >= start_date) & (df["dateAdded"] <= end_date)]
 
+else:
+    # Filter the DataFrame to only include rows where the cveID, vendorProject, or product matches the input
+    filtered_df = df[(df["cveID"] == search_input) | (df["vendorProject"] == search_input) | (df["product"] == search_input)]
+
+# Print the filtered records
+print(filtered_df[["cveID", "vendorProject", "product", "dateAdded"]])
